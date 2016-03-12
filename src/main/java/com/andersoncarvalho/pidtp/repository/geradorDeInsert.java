f8407a4c8ecec1ee3064c764cb4bf2951ac366df
Anderson Carvalho <anderson@andersoncarvalho.com>
2016-03-12 15:04:44 -0300

    Banco de dados automatizado e usuarios do alexei e ramon criados

--- src/main/java/com/andersoncarvalho/pidtp/repository/geradorDeInsert.java
+++ src/main/java/com/andersoncarvalho/pidtp/repository/geradorDeInsert.java
@@ -18,7 +18,9 @@ public class geradorDeInsert {
             BufferedImage bimg = ImageIO.read(new File("/Users/anderson/Projetos/pidtp/src/main/webapp/assets/exemplos/" + i +".jpg"));
             int width          = bimg.getWidth();
             int height         = bimg.getHeight();
-            System.out.println("insert into imagem VALUES ("+ i + ", '../../../assets/exemplos/"+ i + ".jpg', '" + i + "', " + width + ", " + height + ");");
+            System.out.println(i + ";../../../assets/exemplos/"+ i + ".jpg;" + i + ";" + width + ";" + height);
+//            Para gerar para inserir o sql direto descomente a linha abaixo
+//          System.out.println("insert into imagem VALUES ("+ i + ", '../../../assets/exemplos/"+ i + ".jpg', '" + i + "', " + width + ", " + height + ");");
         }
         System.out.println("AWWWWW");
     }
